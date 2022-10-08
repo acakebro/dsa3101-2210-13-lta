4fddf831ca399b7037d99720a3d9b455e239abdc
Lzj <jay260399@gmail.com>
2022-10-09 00:34:37 +0800

    adding local datasets

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -2,9 +2,10 @@
  # @ Create Time: 2022-10-08 23:39:32.168931
 '''
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, dash_table
 import plotly.express as px
 import pandas as pd
+from dash.dependencies import Input, Output
 
 app = Dash(__name__, title="frontend")
 
@@ -19,7 +20,23 @@ df = pd.DataFrame({
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
+traffic_incidents = pd.read_csv("traffic_incidents.csv")
+traffic_speedbands = pd.read_csv("traffic_speedbands.csv")
+traffic_images = pd.read_csv("traffic_images.csv")
+
+
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+d_table = dash_table.DataTable(
+            data=traffic_incidents.to_dict('records'),
+            columns = [{"name": i, "id": i} for i in traffic_incidents.columns[:len(traffic_incidents.columns)-1]],
+            cell_selectable=False,
+            sort_action='native',
+            filter_action='native',
+            page_action = 'native',
+            page_current = 0,
+            page_size = 10
+            )
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -31,8 +48,15 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='example-graph',
         figure=fig
-    )
+    ),
+    html.Div(children = [html.H2("Traffic Incidents"),
+                         d_table],
+             style = {'width':'1000px', 'height':'350px', 'margin':'10px auto', 'padding-right':'30px'})
+    
 ])
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
