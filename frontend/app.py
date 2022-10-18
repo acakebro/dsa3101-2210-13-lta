64618b345669fe3a6ec8beee1ff6070cbafa85e6
XIAOHAN33 <110147257+XIAOHAN33@users.noreply.github.com>
2022-10-18 18:22:58 +0800

    part 1 map
    open terminal, put all these files in the same directory.
    create a new folder then activate virtual environment, pip install wheel, pip install greppo geopandas
    run --- greppo serve app.py
    open localhost 8080, u will see the map

---
+++ frontend/app.py
@@ -0,0 +1,47 @@
+# inside app.py
+# need to upgrade shapely to 1.8.5
+
+from greppo import app
+import geopandas as gpd
+
+app.base_layer(
+    name="Open Street Map",
+    visible=True,
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    subdomains=None,
+    min_zoom = 11,
+    max_zoom = 12,
+    attribution='(C) OpenStreetMap contributors',
+)
+
+app.base_layer(provider="CartoDB Positron")
+
+regions = gpd.read_file("/Users/xiaohan/dsa3101-2210-13-lta/frontend/boundary.geojson")
+roads = gpd.read_file("/Users/xiaohan/dsa3101-2210-13-lta/frontend/singapore_roads_without_residential_unclassified_tertiary_with_link_secondary_with_link_primary_with_link.geojson")
+cameras = gpd.read_file("/Users/xiaohan/dsa3101-2210-13-lta/frontend/singapore_camera.geojson")
+
+app.vector_layer(
+    data = regions,
+    name = "Regions of Singapore",
+    description = "Polygons showing the boundaries of regions of Singapore.",
+    style = {"fillColor": "#4daf4a"},
+)
+
+app.vector_layer(
+    data = roads,
+    name = "Highways in Singapore",
+    description = "Lines showing the major highways in Singapore.",
+    style = {"color": "#377eb8"},
+)
+
+app.vector_layer(
+    data = cameras,
+    name = "Road Cameras of Singapore",
+    description = "Points showing the cameras in Singapore.",
+    style = {"color": "#e41a1c"},
+    visible = True,
+)
+          
+      
+      
+
