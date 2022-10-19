7ae4f324b976bf97d8a5f3c4e6f9ea9f59be4e52
Jayson Lam <e0550546@u.nus.edu>
2022-10-19 18:43:24 +0800

    waiting for timestamp to add graph

--- frontend/src/app2.py
+++ frontend/src/app2.py
@@ -1,3 +1,4 @@
+import pandas as pd 
 import dash
 import os
 from dash import dcc
@@ -7,11 +8,12 @@ import pandas as pd
 import numpy as np
 from os import listdir
 from dash.dependencies import Input, Output
-from datetime import datetime, date
+from datetime import datetime, date,timedelta
 
 image_folder="assets"
 directory = os.fsencode(image_folder)
-'''
+data = pd.read_csv("train_data.csv") 
+
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
@@ -40,7 +42,8 @@ app.layout = html.Div(
         ],
         style = {'width':'100%','display':'flex','align-items':'center','justify-content':'center'}
         ),
-    
+
+    #2nd row dropdowns
     html.Div(
         children=[
         #Camera ID dropdown
@@ -60,9 +63,10 @@ app.layout = html.Div(
             ],
             style={'margin':'20px'}
             ),
+
+        #Date pick
         html.Div(
             children=[
-            #Date pick
             html.Label(['Date:'], style={'font-weight': 'bold'}),
             dcc.DatePickerSingle(id = "traffic_date",
                                  min_date_allowed = date(2022,1,1),
@@ -72,9 +76,10 @@ app.layout = html.Div(
                                  style = {'display': 'inline-block', 'width':'150px','margin':'20px'}
                                  )
             ]),
+
+        #Time input
         html.Div(
             children=[
-            #Time input
             html.Label(['Time:'], style={'font-weight': 'bold'}),
             dcc.Input(id="traffic_time",
                       type="number",
@@ -88,7 +93,8 @@ app.layout = html.Div(
     
     html.Br(),
     html.Br(),
-    
+
+    #Image and predictions
     html.Div(
             children=[
             #Image after filter
@@ -106,23 +112,28 @@ app.layout = html.Div(
                 )
             ],
             style = {'display':'flex','align-items':'center','justify-content':'center'}
-            )
-    ])
+            ),
 
-    #Graphs
-#    html.Div(children = [
-#    dcc.Graph(
-#        id='example-graph1',
-#        figure=fig,
-#        style = {'display': 'inline-block', 'width': '450px'}
-#    ),
-#    
-#    dcc.Graph(
-#        id='example-graph2',
-#        figure=fig,
-#        style = {'display': 'inline-block', 'width': '450px'}
-#    ),
-#    
+
+    #Data visualization
+    html.Div(children = [
+        html.Label(['Past data analysis'], style={'font-weight': 'bold'}),
+        dcc.Dropdown(id='timeframe',
+                        options=[
+                            {'label':'last 30 minutes', 'value':'30'},
+                            {'label':'last 1 hour', 'value':'60'}
+                            ]),
+        dcc.Graph(
+            id='speed',
+            style = {'display': 'inline-block', 'width': '450px'}
+        ),       
+        dcc.Graph(
+            id='density',
+            style = {'display': 'inline-block', 'width': '450px'}
+        )
+        ])
+        ])
+        
 #    dcc.Graph(
 #        id='example-graph3',
 #        figure=fig,
@@ -140,17 +151,23 @@ app.layout = html.Div(
 #             ),
 
 
+
+
+#Enter camera id,date,time and timerange to find speed and density over time of past data
 @app.callback(
 [Output('img','children'),
-Output('attributes','style')],
+ Output('attributes','style'),
+ Output('speed','graph'),
+Output('density','graph')],
 [Input('camera_id','value'),
- Input('traffic_date','date'),
- Input('traffic_time','value')])
+Input('traffic_date','date'),
+Input('traffic_time','value'),
+ Input('timeframe','value')])
 
-def update_output(camera_id,traffic_value,time):
+def update_plot(camera_id,traffic_date,time,timeframe):
     #Stop update if missing values
-    if traffic_value is not None:
-        date_object = date.fromisoformat(traffic_value)
+    if traffic_date is not None:
+        date_object = date.fromisoformat(traffic_date)
         datetime = date_object.strftime('%Y%m%d')
     if camera_id is None or datetime is None:
         raise dash.exceptions.PreventUpdate
@@ -166,7 +183,27 @@ def update_output(camera_id,traffic_value,time):
         if camera_id in file and datetime not in file:
             raise dash.exceptions.PreventUpdate
         if camera_id in file and datetime in file:
-            return [html.Img(src=image_folder+'/'+file)],attributes_style
-
+            img=[html.Img(src=image_folder+'/'+file)]
+    if timeframe:
+        variables=data.copy(deep=True)
+        variables = variables.astype({'timestamp':'str','camera_id':'str'})
+        variables['timestamp']=variables['timestamp'].str.slice(0,12)
+        date_object = datetime.strptime(traffic_date, "%Y-%m-%d")
+        time_object = datetime.strptime(time,'%H%M').time()  
+        datetime_curr = datetime.combine(date_object, time_object)
+        datetime_prev = datetime_curr - timedelta(hours=0, minutes=int(timeframe))
+        datetime_curr = datetime.strftime(datetime_curr, "%Y%m%d%H%M")
+        datetime_prev = datetime.strftime(datetime_prev, "%Y%m%d%H%M")
+        variables = variables[variables['camera_id'] == camera_id ]
+        variables = variables[variables['timestamp'] <= datetime_curr]
+        variables = variables[variables['timestamp'] >= datetime_prev]
+    return img,attributes_style,None,None
+        #speedInput=
+        #densityInput=
+        #speedplot = px.line(graphInput, x='time', y='average_speed (km/h)')
+        #densityplot = px.line(graphInput, x='time', y='density')
+        #return speedplot,densityplot
+    
 if __name__ == '__main__':
     app.run_server(debug=True,port=8052)
+
