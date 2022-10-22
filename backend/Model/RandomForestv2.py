205bfaefd76b7e063f5c9c95739ebf5b4618e7f8
757cyh <67052883+acakebro@users.noreply.github.com>
2022-10-22 17:57:12 +0800

    Applied OOP styling above pipeline

--- backend/Model/RandomForestv2.py
+++ backend/Model/RandomForestv2.py
@@ -7,6 +7,68 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_curve, auc
 
+
+class RandomForestModel:
+    def __init__(self, training, test):
+        self.training = pd.read_csv("training_data.csv")
+        self.test = pd.read_csv("test_data.csv")
+
+    def toDummy(self, data):
+        time = self.training.pop("Time")  # remove time
+        # date = self.training.pop("Date")
+        for col in data.dtypes[data.dtypes == "object"].index:
+            for_dummy = data.pop(col)
+            data = pd.concat([data, pd.get_dummies(for_dummy, prefix=col)], axis=1)
+
+    def getModel(self):  # no pickle
+        labels = self.training.pop("Jam")
+        x_train, x_test, y_train, y_test = train_test_split(
+            self.training, labels, test_size=0.25
+        )
+        model = RandomForestClassifier()
+        model.fit = RandomForestClassifier()
+        y_pred = model.predict(x_test)
+        false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred)
+        roc_auc = auc(false_positive_rate, true_positive_rate)
+
+        n_estimators = [1, 2, 4, 8, 16, 32, 64, 100, 200]
+        train_results = []
+        test_results = []
+        for estimator in n_estimators:
+            rf = RandomForestClassifier(n_estimators=estimator, n_jobs=-1)
+            rf.fit(x_train, y_train)
+            train_pred = rf.predict(x_train)
+            false_positive_rate, true_positive_rate, thresholds = roc_curve(
+                y_train, train_pred
+            )
+            roc_auc = auc(false_positive_rate, true_positive_rate)
+            train_results.append(roc_auc)
+            y_pred = rf.predict(x_test)
+            false_positive_rate, true_positive_rate, thresholds = roc_curve(
+                y_test, y_pred
+            )
+            roc_auc = auc(false_positive_rate, true_positive_rate)
+            test_results.append(roc_auc)
+
+        index = test_results.index(min(test_results))
+        best_n = n_estimators[index]
+        model = RandomForestClassifier(n_estimators=best_n)
+        model.fit(self.training, labels)
+        # pickle.dump(model, open("model.pkl", "wb"))
+
+    def predict_(self):
+        copy = self.test.copy()
+        testTime = self.test.pop("Time")
+        testDate = self.test.pop("Date")
+        self.toDummy(self.test)
+        test_pred = self.getModel().predict(self.test)
+        copy["Jam"] = test_pred
+        copy.to_csv("Final_output.csv")
+
+
+# ----------------------------PIPELINE-----------------------------------------------
+
+
 # Loading of datasets
 train = pd.read_csv("training_data.csv")
 
