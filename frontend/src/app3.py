a28335e9f3253400f69d561da3b34dc9146b11b7
XIAOHAN33 <110147257+XIAOHAN33@users.noreply.github.com>
2022-10-27 17:54:47 +0800

    Update app3.py

--- frontend/src/app3.py
+++ frontend/src/app3.py
@@ -11,6 +11,8 @@ from time import localtime, strftime
 import dash_bootstrap_components as dbc
 from dash import Dash, html, dcc
 
+data = pd.read_csv("train_data.csv")
+
 app = Dash(__name__, title="frontend")
 
 app.layout = html.Div(
@@ -23,9 +25,9 @@ app.layout = html.Div(
         html.H3('Select camera', style={'font-weight': 'bold'}),
         dcc.Dropdown(id='camera_id',
                         placeholder='1001',
-                        style={'width':'170px', 'margin':'10px','display': 'inline-block'})
+                        style={'width':'135px','margin':'0 auto', 'display': 'inline-block'})
             ],
-        style = {'width':'100%','display':'flex','align-items':'center','justify-content':'center'}),
+        style = {'width':'33.8%','margin-left':'20px','display':'flex','align-items':'center','justify-content':'center'}),
     html.Div(
         # Road dropdown
         children = [
@@ -43,9 +45,10 @@ app.layout = html.Div(
                 {'label':'TPE', 'value':'TPE'},
                 {'label':'AYE', 'value':'AYE'}],
                 placeholder="Select road...",
-                style={'width':'150px','margin':'20px'})
+                style={'width':'134px','margin':'0 auto'})
         ],
-        style = {'width':'100%','display':'flex','align-items':'center','justify-content':'center'}),
+        style = {'width':'35.4%','display':'flex','align-items':'center','justify-content':'center',
+                 'margin-left':'20px'}),
 
     #Date pick
     html.Div(
@@ -54,12 +57,12 @@ app.layout = html.Div(
         dcc.DatePickerSingle(id = "traffic_date",
                                 min_date_allowed = date.today(),
                                 max_date_allowed = date.today()+timedelta(days=30),
-                                # date = date.today(),
+                                date = date.today(),
                                 initial_visible_month = date.today(),
                                 placeholder='DD/MM/YYYY',
-                                style = {'width':'150px','margin':'20px'})
+                                style = {'width':'150px','margin':'0 auto', 'border-radius': '0 auto'})
         ],
-        style ={'width':'100%','display':'flex','align-items':'center','justify-content':'center'}),
+        style ={'margin-left':'20px','width':'35.5%','display':'flex','align-items':'center','justify-content':'center'}),
 
     #Time input
     html.Div(
@@ -68,10 +71,18 @@ app.layout = html.Div(
         dcc.Input(id="traffic_time",
                       type="text",
                       placeholder="HHMM",
-                      #value=strftime("%H%M", localtime()),
-                      style = {'display': 'inline-block', 'width':'100px','height':'30px','margin':'25px'})
+                      value=strftime("%H%M", localtime()),
+                      style = {'width':'126px','margin':'0 auto', 'height':'30px', 'display': 'inline-block'})
         ],
-        style = {'width':'100%','display':'flex','align-items':'center','justify-content':'center'}),
+        style = {'width':'29.3%','margin-left':'20px','display':'flex','align-items':'center','justify-content':'center'}),
+
+    # predictions
+    html.Div(
+        children=[
+        html.H3("Prediction for the road condition.", style = {'font-weight': 'bold'})],
+        style={'display':'none','padding':'20px'}, id = 'attributes')
+
+    
     ], style={'text-align':'center', 'display':'inline-block', 'width':'100%'})
 
 @app.callback(
@@ -81,10 +92,29 @@ Input('road_name','value'))
 def update_camera(road_name):
     df=data[data["Direction"]==road_name]
     return [{'label': i, 'value': str(i)} for i in df['camera_id'].unique()]
-    # predictions
-    #html.H3("Here is the prediction for the road condition.")
+
+#Enter camera id,date,time and timerange to find speed and density over time of past data
+@app.callback(
+Output('attributes','value'),
+[Input('traffic_time','value'),
+ Input('traffic_date','date'),
+ Input('camera_id','value'),
+ Input('road_name','value')])
+
+def make_prediction():
+    time = request.args.get('traffic_time')
+    date = request.args.get('traffic_date')
+    camera_id = int(request.args.get('camera_id'))
+    road = request.args.get('road_name')
+    model = pickle.load(open("model.pkl", "rb"))
+    result = model.predict(camera_id, road, date, time)
+    if result == 0:
+        return jsonify({'prediction': 'No Jam'})
+    elif result == 1:
+        return jsonify({'prediction': 'Jam'})
 
                          
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8054)
+    app.run_server(debug=True, port=8055)
+
