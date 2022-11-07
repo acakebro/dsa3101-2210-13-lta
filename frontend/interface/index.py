67954888ad2fa5dee9cfc0f8bf338bd446043bbb
XIAOHAN33 <110147257+XIAOHAN33@users.noreply.github.com>
2022-11-07 21:57:46 +0800

    Update index.py

--- frontend/interface/index.py
+++ frontend/interface/index.py
@@ -85,7 +85,7 @@ app.layout = html.Div([
 
 
 if __name__ == '__main__':
-    #app.run_server(host='0.0.0.0',debug=True, port=8050)
-    app.run_server(debug=True, port = 8051)
+    app.run_server(host='0.0.0.0',debug=True, port=8050)
+    # app.run_server(debug=True, port = 8051)
     
 
